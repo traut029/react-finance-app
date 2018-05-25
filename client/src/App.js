e92df6fd141548c3085013e12136d16e2eff49cb
mguldberg <guldberg@sbcglobal.net>
2018-05-25 00:23:50 -0500

    got the login page working in REACT

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/LoginPage";
 import ExpensesPage from "./pages/ExpensesPage";
 import InvestmentsPage from "./pages/InvestmentsPage";
 // import NoMatch from "./pages/NoMatch";
 // import Footer from './components/Footer';
+import './App.css';
+
 
 const App = () => (
   <div>
     <Router>
       <Switch>
-        {/* <Route exact path="/" component={LoginPage} /> */}
+        <Route exact path="/" component={LoginPage} />
         <Route exact path="/expenses" component={ExpensesPage} />
         <Route exact path="/investments" component={InvestmentsPage} />
         {/* <Route component={NoMatch} /> */}
